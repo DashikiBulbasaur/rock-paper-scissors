8976c353836ff8876f709450afcf838319f34079
DashikiBulbasaur <githubearl@gmail.com>
2025-07-23 23:04:28 -0600

    add getHumanChoice function

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -10,4 +10,16 @@ function getComputerChoice() {
     }
 }
 
-console.log(getComputerChoice());
+function getHumanChoice() {
+    let humanChoice = prompt("Rock? Paper? Scissors? Choose one. Note: only the strings 'rock', 'paper', and 'scissors' will be accepted.");
+
+    if (humanChoice === "rock") {
+        return "rock";
+    } else if (humanChoice === "paper") {
+        return "paper";
+    } else {
+        // since no error handling is involved (for now), this block assumes the 
+        // choice is scissors 
+        return "scissors";
+    }
+}
