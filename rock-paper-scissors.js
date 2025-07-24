14e34fb5e93581177d48e4d4d2892d7bd4864ad9
DashikiBulbasaur <githubearl@gmail.com>
2025-07-24 00:01:53 -0600

    add playRound function and the process to actually play the game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,14 +12,90 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = prompt("Rock? Paper? Scissors? Choose one. Note: only the strings 'rock', 'paper', and 'scissors' will be accepted.");
+    humanChoice = humanChoice.toLowerCase();
 
     if (humanChoice === "rock") {
         return "rock";
     } else if (humanChoice === "paper") {
         return "paper";
-    } else {
-        // since no error handling is involved (for now), this block assumes the 
-        // choice is scissors 
+    } else if (humanChoice === "scissors") {
         return "scissors";
+    } else {
+        // if the input is invalid, just make the computer win
+        return "";
     }
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        // i don't like the nested switch statements. ideally, the computerChoice switches
+        // would be its own separate function
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    console.log("You and the computer both chose rock! No one wins!");
+                    console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
+                    break;
+                case "paper":
+                    console.log("The computer chose paper! The computer wins!");
+                    computerScore++;
+                    console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
+                    break;
+                case "scissors":
+                    console.log("The computer chose scissors! You win!");
+                    humanScore++;
+                    console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
+                    break;
+            }
+            break;
+        case "paper":
+            switch (computerChoice) {
+                case "paper":
+                    console.log("You and the computer both chose paper! No one wins!");
+                    console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
+                    break;
+                case "scissors":
+                    console.log("The computer chose scissors! The computer wins!");
+                    computerScore++;
+                    console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
+                    break;
+                case "rock":
+                    console.log("The computer chose rock! You win!");
+                    humanScore++;
+                    console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (computerChoice) {
+                case "scissors":
+                    console.log("You and the computer both chose scissors! No one wins!");
+                    console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
+                    break;
+                case "rock":
+                    console.log("The computer chose rock! The computer wins!");
+                    computerScore++;
+                    console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
+                    break;
+                case "paper":
+                    console.log("The computer chose paper! You win!");
+                    humanScore++;
+                    console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
+                    break;
+            }
+            break;
+        default:
+                console.log("Your choice was invalid! Therefore, the computer wins!");
+                computerScore++;
+                console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
+            break;
+    }
+}
+
+const getComputerSelection = getComputerChoice();
+const getHumanSelection = getHumanChoice();
+
+playRound(getHumanSelection, getComputerSelection);
