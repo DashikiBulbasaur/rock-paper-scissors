5684ca51cce547e8fe5a6619fe25e1d8d3424e27
DashikiBulbasaur <githubearl@gmail.com>
2025-07-24 20:10:30 -0600

    remove first to three wins functionality since it didn't work. add limit of 5 games and state who's the winner functionality, as suggested by the odin project

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -12,6 +12,9 @@ function getComputerChoice() {
 
 function getHumanChoice() {
     let humanChoice = prompt("Rock? Paper? Scissors? Choose one. Note: only the strings 'rock', 'paper', and 'scissors' will be accepted.");
+    if (humanChoice === null) {
+        return "";
+    }
     humanChoice = humanChoice.toLowerCase();
 
     if (humanChoice === "rock") {
@@ -38,19 +41,19 @@ function playRound(humanChoice, computerChoice) {
                 case "rock":
                     console.log("You and the computer both chose rock! No one wins!");
                     console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
-                    console.log("First to three wins is the winner");
+                    console.log("");
                     break;
                 case "paper":
                     console.log("The computer chose paper! The computer wins!");
                     computerScore++;
                     console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
-                    console.log("First to three wins is the winner");
+                    console.log("");
                     break;
                 case "scissors":
                     console.log("The computer chose scissors! You win!");
                     humanScore++;
                     console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
-                    console.log("First to three wins is the winner");
+                    console.log("");
                     break;
             }
             break;
@@ -59,19 +62,19 @@ function playRound(humanChoice, computerChoice) {
                 case "paper":
                     console.log("You and the computer both chose paper! No one wins!");
                     console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
-                    console.log("First to three wins is the winner");
+                    console.log("");
                     break;
                 case "scissors":
                     console.log("The computer chose scissors! The computer wins!");
                     computerScore++;
                     console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
-                    console.log("First to three wins is the winner");
+                    console.log("");
                     break;
                 case "rock":
                     console.log("The computer chose rock! You win!");
                     humanScore++;
                     console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
-                    console.log("First to three wins is the winner");
+                    console.log("");
                     break;
             }
             break;
@@ -80,19 +83,19 @@ function playRound(humanChoice, computerChoice) {
                 case "scissors":
                     console.log("You and the computer both chose scissors! No one wins!");
                     console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
-                    console.log("First to three wins is the winner");
+                    console.log("");
                     break;
                 case "rock":
                     console.log("The computer chose rock! The computer wins!");
                     computerScore++;
                     console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
-                    console.log("First to three wins is the winner");
+                    console.log("");
                     break;
                 case "paper":
                     console.log("The computer chose paper! You win!");
                     humanScore++;
                     console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
-                    console.log("First to three wins is the winner");
+                    console.log("");
                     break;
             }
             break;
@@ -100,19 +103,24 @@ function playRound(humanChoice, computerChoice) {
                 console.log("Your choice was invalid! Therefore, the computer wins!");
                 computerScore++;
                 console.log(`Your score: ${humanScore}. The computer's score: ${computerScore}.`);
-                console.log("First to three wins is the winner");
+                console.log("");
             break;
     }
 }
 
-while (humanScore < 3 || computerScore < 3) {
+let number_of_games = 0;
+
+while (number_of_games <= 5) {
+    number_of_games++;
     const getComputerSelection = getComputerChoice();
     const getHumanSelection = getHumanChoice();
     playRound(getHumanSelection, getComputerSelection);
 }
 
-if (computerScore === 3) {
-    console.log("The computer won!");
+if (computerScore === humanScore) {
+    console.log("After 5 games, you and the computer are tied!");
+} else if (computerScore > humanScore) {
+    console.log("After 5 games, the computer won more games than you!");
 } else {
-    console.log("You won! Congratulations!");
+    console.log("After 5 games, you won more games than the computer!");
 }
